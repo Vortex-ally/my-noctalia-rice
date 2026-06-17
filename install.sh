4e715d7f084c1e7fa0bb2e661eaa801ea1dee6df
vortex-ally <pertsovychruslan@gmail.com>
2026-06-17 22:43:57 +0300

    change install.sh file

--- install.sh
+++ install.sh
@@ -1,14 +1,18 @@
-# install base system
-sudo pacman -S --needed - < packages.txt
+#!/bin/bash
 
-# install AUR
-yay -S --needed - < aur-packages.txt
+set -e
 
-# copy configs
+echo "Installing official packages..."
+sudo pacman -S --needed $(cat packages.txt)
+
+if [ -s aur-packages.txt ]; then
+    echo "Installing AUR packages..."
+    yay -S --needed $(cat aur-packages.txt)
+fi
+
+echo "Copying configs..."
+mkdir -p ~/.config
 cp -r configs/* ~/.config/
 
-# install noctalia plugins (ВАЖЛИВО)
-while read plugin; do
-    echo "Installing $plugin..."
-    noctalia install-plugin "$plugin"
-done < plugins.txt
+echo "Done!"
+echo "Reboot recommended."
